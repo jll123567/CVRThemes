458d8aee4b4171bfe05cd66f5a8d87954fb25702
JillTheSomething <ledbetterte@gmail.com>
2024-09-03 23:26:04 -0400

    Inital Commit

---
+++ CVRThemes/CVRThemes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using MelonLoader;
+using BTKUILib;
+
+namespace CVRThemes
+{
+    public class Main: MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            string gameDir = Directory.GetCurrentDirectory();
+            string themesDir = gameDir + "\\UserData\\CVRThemes";
+
+            string quickCSS;
+
+            try  // Try and get the custom css and load it.
+            {
+                
+                using (StreamReader stream = new StreamReader(themesDir+"\\quick.css") ?? throw new InvalidOperationException())
+                {
+                    quickCSS = stream.ReadToEnd();
+                }
+
+                QuickMenuAPI.InjectCSSStyle(quickCSS);
+                LoggerInstance.Msg("Loaded and applied theme!");
+            }
+            catch (FileNotFoundException)
+            {
+                LoggerInstance.Error("Failed to find `quick.css`.\nDid you install the themes correctly to ChilloutVR/UserData/CVRThemes?");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Directory.CreateDirectory(themesDir);
+                LoggerInstance.Error("Failed to find CVRThemes folder, so I just made it in <CVR Install>/UserData/CVRThemes.\n Put your themes here and restart the game.");
+            }
+            catch (InvalidOperationException)
+            {
+                LoggerInstance.Error("Unknown Error: Tell JillTheSomething to figure out why `StreamReader stream = new StreamReader(gameDir + <CSSPath>)` returned null.\n");
+            }
+
+        }
+    }
+}
---
+++ CVRThemes/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using MelonLoader;
+using CVRThemes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: MelonInfo(typeof(CVRThemes.Main),
+    "CVRThemes",
+    "0.0.1",
+    "JillTheSomething")]
+[assembly: MelonColor(255,249,190,0)]
+[assembly: MelonAuthorColor(255, 0, 255, 0)]
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonAdditionalDependencies("BTKUILib")]
